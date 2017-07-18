131562bb32e36abc8ba913e4983ab62fd5aa2a49
AlexB <ua3mqj@gmail.com>
2017-07-19 00:33:48 +0300

    make app

---
+++ lib/mmql.ex
@@ -0,0 +1,18 @@
+defmodule Mmql do
+  @moduledoc """
+  Documentation for Mmql.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Mmql.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
